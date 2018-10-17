e9f4a1ee19d7b38be47cb728bc2302050c68380e
m.deljavan <mohammadrezadeljavan@gmail.com>
2018-10-17 13:43:53 +0330

    solve works

--- mathString.ts
+++ mathString.ts
@@ -13,84 +13,93 @@ class MathStrign {
   );
   multiplicationOrDivisionRegExp: RegExp = new RegExp(
     `${this.numRegString}[\\${this.mathSigns.multiplication}\\${this.mathSigns.divisin}]${this.numRegString}`
-  );
+    , 'g');
   summationOrSubtractionRegExp: RegExp = new RegExp(
     `${this.numRegString}[\\${this.mathSigns.summation}\\${this.mathSigns.subtraction}]${this.numRegString}`,
     'g'
   );
-
-  constructor(public input: string) {
-    this.input = input.replace(/\s*/g, '');
-  }
   selectNumbers = (input: string) => {
-    return input.replace(/\s*/g,'').match(this.numberRegExp);
+    return input.replace(/\s*/g, '').match(this.numberRegExp);
   };
   selectParantheses = (input: string) => {
-    return input.replace(/\s*/g,'').match(this.betweenParenthesesRegExp);
+    return input.replace(/\s*/g, '').match(this.betweenParenthesesRegExp);
   };
   selectMultiplicationOrDivisionePhrase = (input: string) => {
-    return input.replace(/\s*/g,'').match(this.multiplicationOrDivisionRegExp);
+    return input.replace(/\s*/g, '').match(this.multiplicationOrDivisionRegExp);
   };
   selectSummationOrSubtractionPhrase = (input: string) => {
-    return input.replace(/\s*/,'').match(this.summationOrSubtractionRegExp);
+    return input.replace(/\s*/g, '').match(this.summationOrSubtractionRegExp);
   };
+  converSpecialChar = (inp: string) => {
+   return `${inp
+    .replace(/\(/g,'\\(')
+    .replace(/\)/g,'\\)')
+    .replace(/\\/g, '\\/')
+    .replace(/\+/g, '\\+')
+    .replace(/\-/g, '\\-')
+    .replace(/\*/g, '\\*')}`
+  }
   solveBetweenParantheses = (input: string): string => {
-    let betweenParantes = this.selectParantheses(input);
     let result: string = input;
+    let betweenParantes = this.selectParantheses(result);
     if (!betweenParantes) {
       return result;
     }
     betweenParantes.forEach(inp => {
-     const resultPhrase = this.solveMathPhrases(inp);
-     result = input.replace(new RegExp(`${inp}`), resultPhrase)
+      const resultPhrase = this.solveMathPhrases(inp);
+      result = result.replace(inp, resultPhrase)
     });
     return this.solveBetweenParantheses(result)
   };
   solveMathPhrases = (phrase: string) => {
-    let result: string = phrase;
-    const multiplicationOrDivisionPhrase = this.selectMultiplicationOrDivisionePhrase(
-      result
-    );
-    if (multiplicationOrDivisionPhrase) {
-      multiplicationOrDivisionPhrase.forEach(phr => {
-        const _result = this.solveMultiplicationOrDivision(phr);
-        result = result.replace(new RegExp(`${phr.replace('/','\\/').replace('+','\\+').replace('*','\\*')}`), _result);
-      });
-    }
-    const summationOrSubtractionPhrase = this.selectSummationOrSubtractionPhrase(result)
-    if (summationOrSubtractionPhrase) {
-      summationOrSubtractionPhrase.forEach(phr => {
-        const _result = this.solveSummationOrSubtraction(phr)
-        result = result.replace(new RegExp(`${phr.replace('+','\\+')}`), _result)
-        console.log(result)
-      })
-
-    }
-    result = result.replace(/[()]/,'');
+    let result: string = this.solveMultiplicationOrDivisionPhrase(phrase)
+    result = this.solveSummationOrSubtractionPhrase(result);
+    result = result.replace(/[()]/g, '');
     return result
   };
-  solveMultiplicationOrDivision = (phrase: string) => {
-    const numbers = this.selectNumbers(phrase)!;
-    const num1 = numbers[0];
-    const num2 = numbers[1];
-    const result = phrase.includes(this.mathSigns.multiplication)
-      ? +num1 * +num2
-      : +num1 / +num2;
-
-    return result >= 0 ? `+${result.toString()}` : result.toString();
-  };
-  solveSummationOrSubtraction = (phrase: string) => {
-    const numbers = this.selectNumbers(phrase)!
-    const num1 = numbers[0]
-    const num2 = numbers[1]
-    const result = phrase.includes(this.mathSigns.summation) ? +num1 + +num2 : +num1 - +num2
-    return result >= 0 ? `+${result.toString()}` : result.toString()
+  solveMultiplicationOrDivisionPhrase = (phrase: string): string => {
+    const multiplicationOrDivisionPhrase = this.selectMultiplicationOrDivisionePhrase(
+      phrase
+    );
+    if (!multiplicationOrDivisionPhrase) return phrase
+    multiplicationOrDivisionPhrase.forEach(phr => {
+      const _result = this.solveMultiplicationOrDivision(phr);
+      phrase = phrase.replace(phr, _result);
+    });
+    return this.solveMultiplicationOrDivisionPhrase(phrase)
   }
-  solve = () => {
-    let result = this.input;
-    result = this.solveBetweenParantheses(result);
-    result = this.solveMathPhrases(result);
-    return result;
-  };
+  solveSummationOrSubtractionPhrase = (phrase: string): string => {
+    phrase = phrase.replace(/(\-\+)|(\+\-)/g,'-').replace(/(\+\+)|(\-\-)/g,'+');
+    const summationOrSubtractionPhrase = this.selectSummationOrSubtractionPhrase(phrase)
+    if (!summationOrSubtractionPhrase) return phrase;
+    summationOrSubtractionPhrase.forEach(phr => {
+      const _result = this.solveSummationOrSubtraction(phr)
+      phrase = phrase.replace(phr, _result)
+    })
+    return this.solveSummationOrSubtractionPhrase(phrase)
+}
+solveMultiplicationOrDivision = (phrase: string) => {
+  const numbers = this.selectNumbers(phrase)!;
+  const num1 = numbers[0];
+  const num2 = numbers[1];
+  const result = phrase.includes(this.mathSigns.multiplication)
+  ? +num1 * +num2
+  : +num1 / +num2; 
+  return result >= 0 ? `+${result.toString()}` : result.toString();
+};
+solveSummationOrSubtraction = (phrase: string) => {
+  const numbers = this.selectNumbers(phrase)!;
+  const num1 = numbers[0]
+  const num2 = numbers[1]
+  const mathOperator = phrase.replace(new RegExp(`(${this.converSpecialChar(num1)}|${this.converSpecialChar(num2)})`,'g'),'')
+  const result = (mathOperator && mathOperator == this.mathSigns.subtraction) ? +num1 - +num2 : +num1 + +num2
+  return result >= 0 ? `+${result.toString()}` : result.toString()
+}
+solve = (input: string) => { 
+  let result = input.replace(/\s*/g, '');
+  result = this.solveBetweenParantheses(result);
+  result = this.solveMathPhrases(result);
+  return result;
+};
 }
 export default MathStrign;
