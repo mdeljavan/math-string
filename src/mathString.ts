afc716ab4a557bdefcd444beb11c388d4b608ec7
m.deljavan <mohammadrezadeljavan@gmail.com>
2019-03-07 15:36:31 +0330

    add check input to mathString

--- src/mathString.ts
+++ src/mathString.ts
@@ -1,6 +1,8 @@
+import { checkInput } from './checkPhrase';
 import { solveBetweenParantheses, solveMathPhrases } from './solveModules';
 
 const solve = (input: string) => {
+  checkInput(input);
   let result = solveBetweenParantheses(input);
   result = solveMathPhrases(result);
   return +result > 0 ? result.replace('+', '') : result;
