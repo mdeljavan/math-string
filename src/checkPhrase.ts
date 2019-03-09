d8d491b8ed05ab636fe2dc338771322b9127c3ed
m.deljavan <mohammadrezadeljavan@gmail.com>
2019-03-09 12:37:35 +0330

    added read me file

--- src/checkPhrase.ts
+++ src/checkPhrase.ts
@@ -17,13 +17,13 @@ const numberOfChar = (inp: string, char: string) => {
   const res = inp.match(regExString);
   return res ? res.length : 0;
 };
-const checkUseMoreThanOne = (inp: string, char: string) => {
-  const regExString = new RegExp(`\\${char}{2,}`, 'g');
+const checkUseMoreThanOne = (inp: string, char: string, quantity: number) => {
+  const regExString = new RegExp(`\\${char}{${quantity},}`, 'g');
   const res = inp.match(regExString);
   return res ? res.length : 0;
 };
-const checkChar = (inp: string, operator: string) => {
-  if (checkUseMoreThanOne(inp, operator) > 0) {
-    throw new Error(`the ${operator} is used more than one in one place`);
+const checkChar = (inp: string, operator: string, quantity: number = 2) => {
+  if (checkUseMoreThanOne(inp, operator, quantity) > 0) {
+    throw new Error(`the ${operator} is used ${quantity} times in one place`);
   }
 };
