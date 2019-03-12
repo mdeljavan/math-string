3a6b3128d23b06186e91f9f2fb9383e72f39eca7
m.deljavan <mohammadrezadeljavan@gmail.com>
2019-03-12 09:00:51 +0330

    rewrite tests and use each function in tests

--- src/mathString.test.ts
+++ src/mathString.test.ts
@@ -1,98 +1,107 @@
 import { checkInput } from './checkPhrase';
 import solve from './mathString';
-import {
-  selectMultiplicationPhrase,
-  selectNumbers,
-  selectParantheses
-} from './selectModules';
+import { selectNumbers, selectParantheses } from './selectModules';
 import {
   solveDivisionPharse,
   solveMathPhrases,
   solveMultiplication,
-  solveSevralPositiveAndNegativeSings
+  solvePowerPhrase,
+  solveSevralPositiveAndNegativeSings,
+  solveSummationOrSubtractionPhrase
 } from './solveModules';
-const operator = ['+', '-', '*', '/'];
-const input: string[] = [
-  '1+11+       +9-11e23',
-  '+(1+1)+(111+(1111)-111)*(1*1/2/5-1+1)',
-  '3*3',
-  '3*-3',
-  '4/2',
-  '1+2-4*2+4/2',
-  '1+2-(4*2)+4/2',
-  '-1+3*((4+4*(3+4))+(3*(-1)*-2)/4)+3-5',
-  '1*2/4*6+4*3',
-  '1-+-+--+2',
-  '1+++2----3--+-+4',
-  '1+2^3+2^(4^2+7)',
-];
+const inputsAndResults = {
+  selectNumbers: [['1+11+       +9-11e23', ['1', '+11', '+9', '-11e23']]],
+  selectParentheses: [
+    [
+      '+(1+1)+(111+(1111)-111)*(1*1/2/5-1+1)',
+      ['(1+1)', '(1111)', '(1*1/2/5-1+1)'],
+    ],
+  ],
+  solvePositivesAndNegatives: [
+    ['1-+-+--+2', '1+2'],
+    ['1+++2----3--+-+4', '1+2+3-4'],
+  ],
+  solveMultiplicationPhrase: [['3*3', '+9'], ['3*-3', '-9']],
+  solveDivisionPhrase: [
+    ['4/2', '4*0.5'],
+    ['4/-2', '4*-0.5'],
+    ['4/2/2', '4*0.5*0.5'],
+  ],
+  solveSummationOrSubtractionPhrase: [
+    ['1+2', '+3'],
+    ['1-2', '-1'],
+    ['-1-2', '-3'],
+    ['-1+2', '+1'],
+  ],
+  solvePowerPhrase: [['1^2', '+1'], ['2^-1', '+0.5']],
+  solve: [
+    ['1+2-4*2+4/2', '-3'],
+    ['1+2-(4*2)+4/2', '-3'],
+    ['-1+3*((4+4*(3+4))+(3*(-1)*-2)/4)+3-5', '97.5'],
+    ['1*2/4*6+4*3', '15'],
+    ['1+2^3+2^(4^2+7)', '8388617'],
+  ],
+};
 const faultInput = [
   '1**1*1***2',
   '1//3/3/////4',
   '1....2+1.2+1...2',
   '1+(2+2+2+3+(3)',
 ];
-describe('test Regex for inputs', () => {
-  it('select numbers', () => {
-    expect(selectNumbers(input[0])).toEqual(['1', '+11', '+9', '-11e23']);
-  });
-  it('select parantesses and numbers between them', () => {
-    expect(selectParantheses(input[1])).toEqual([
-      '(1+1)',
-      '(1111)',
-      '(1*1/2/5-1+1)',
-    ]);
-  });
-  it('select multiple or divide two numbers', () => {
-    expect(selectMultiplicationPhrase(input[1])).toEqual(['1*1']);
+describe('detect fault input and throw error', () => {
+  test.each(faultInput)('throw an error', (phr: string) => {
+    expect(() => checkInput(phr)).toThrow();
   });
 });
-describe('solve phrases math', () => {
-  it('resolve sevral positive and negtive signs', () => {
-    expect(solveSevralPositiveAndNegativeSings(input[9])).toBe('1+2');
-  });
-  it('resolve sevral positive and negtive signs', () => {
-    expect(solveSevralPositiveAndNegativeSings(input[10])).toBe('1+2+3-4');
-  });
-  it('solve multiplication and return possitive', () => {
-    expect(solveMultiplication(input[2])).toBe('+9');
-  });
-  it('solve multiplication and return negative', () => {
-    expect(solveMultiplication(input[3])).toBe('-9');
-  });
-  it('solve division', () => {
-    expect(solveDivisionPharse(input[4])).toBe('4*0.5');
-  });
-  it('solve a phrase without parantheses', () => {
-    expect(solveMathPhrases(input[5])).toBe('-3');
-  });
-  it('solve a phrase with parantheses', () => {
-    expect(solve(input[7])).toBe('97.5');
-    expect(solve(input[8])).toBe('15');
-    expect(solve(input[5])).toBe('-3');
-    expect(solve(input[4])).toBe('2');
-    expect(solve(input[11])).toBe('8388617');
+describe('test select number RegExp ', () => {
+  test.each(inputsAndResults.selectNumbers)(
+    'test select number for %s',
+    (phr: string, expected: string) => {
+      expect(selectNumbers(phr)).toEqual(expected);
+    }
+  );
+});
+describe('select phrase between parentheses', () => {
+  test.each(inputsAndResults.selectParentheses)('%s', (phr, expected) => {
+    expect(selectParantheses(phr)).toEqual(expected);
   });
 });
-describe('detect fault input', () => {
-  it('throw error if use more than one * operator in one place', () => {
-    expect(() => {
-      checkInput(faultInput[0]);
-    }).toThrow();
+describe('conver division to multiplication', () => {
+  test.each(inputsAndResults.solveDivisionPhrase)('%s', (phr, expected) => {
+    expect(solveDivisionPharse(phr)).toEqual(expected);
   });
-  it('throw error if use more than one / operator in one place', () => {
-    expect(() => {
-      checkInput(faultInput[1]);
-    }).toThrow();
+});
+describe('solve a phrase', () => {
+  test.each(inputsAndResults.solve)('%s', (phr, expected) => {
+    expect(solve(phr)).toEqual(expected);
   });
-  it('throw error if use more than one . in one place', () => {
-    expect(() => {
-      checkInput(faultInput[2]);
-    }).toThrow();
+});
+describe('solve a multiplication phrase', () => {
+  test.each(inputsAndResults.solveMultiplicationPhrase)(
+    '%s',
+    (phr, expected) => {
+      expect(solveMultiplication(phr)).toEqual(expected);
+    }
+  );
+});
+describe('resolve several positive and nagative signs', () => {
+  test.each(inputsAndResults.solvePositivesAndNegatives)(
+    '%s',
+    (phr, expected) => {
+      expect(solveSevralPositiveAndNegativeSings(phr)).toEqual(expected);
+    }
+  );
+});
+describe('solve a power phrase', () => {
+  test.each(inputsAndResults.solvePowerPhrase)('%s', (phr, expected) => {
+    expect(solvePowerPhrase(phr)).toEqual(expected);
   });
-  it('throw error if use not equal parenthesses', () => {
-    expect(() => {
-      checkInput(faultInput[3]);
-    }).toThrow();
-  });
+});
+describe('solve a summation or subtraction phrase', () => {
+  test.each(inputsAndResults.solveSummationOrSubtractionPhrase)(
+    '%s',
+    (phr, expected) => {
+      expect(solveSummationOrSubtractionPhrase(phr)).toEqual(expected);
+    }
+  );
 });
